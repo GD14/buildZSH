972634b3777a5a2e8d0b32a69bd28ca315e10290
GD14 <GD14@users.noreply.github.com>
2023-09-19 10:07:11 +0800

    first commit

---
+++ buildZSH.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#安装oh-my-zsh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
+
+#安装zsh-autosuggestions 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#安装zsh-syntax-highlighting
+git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#安装autojump
+git clone https://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+
+#替换主题色
+cp -rf ./gianu.zsh-theme ~/.oh-my-zsh/themes/gianu.zsh-theme
+
+#替换zshrc文件
+cp -rf ./zshrc ~/.zshrc
